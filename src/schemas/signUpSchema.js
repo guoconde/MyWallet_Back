0392b013f2b6c560e1f7db72c6eda0ac720b11ff
Marcio Cunha <marcioabcd11@gmail.com>
2022-02-15 14:49:25 -0300

    Preparando Review

--- src/schemas/signUpSchema.js
+++
@@ -1,9 +0,0 @@
-import joi from "joi"
-
-const signUpSchema = joi.object({
-    name: joi.string().min(2).trim().required('Por favor adicione um nome válido'),
-    email: joi.string().email().required('Por favor adicione um email válido'),
-    password: joi.string().required()
-})
-
-export default signUpSchema
